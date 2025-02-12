06f17453d3a24691bba87586920ced2fbc58c721
Terry Easley <easleter000@warren.k12.in.us>
2025-02-12 12:51:34 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+
+import java.util.HashMap;
+import java.util.Map;
+
+class GFG {
+    public static void main(String[] args)
+    {
+        String str = "Alice is girl and Bob is boy";
+
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        String[] words = str.split(" ");
+
+        for (String word : words) {
+            Integer integer = hashMap.get(word);
+
+            if (integer == null)
+
+                hashMap.put(word, 1);
+
+            else {
+                hashMap.put(word, integer + 1);
+            }
+        }
+        System.out.println(hashMap);
+    }
 }
